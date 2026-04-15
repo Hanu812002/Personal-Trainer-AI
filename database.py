2a5d936469ee93de1d857ecd8697fb4317d9bdce
Hanu Agarwal <87744370+Hanu812002@users.noreply.github.com>
2026-04-15 23:41:47 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,8 +5,8 @@ from datetime import datetime
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = st.secrets("SUPABASE_URL")
+SUPABASE_KEY = st.secrets("SUPABASE_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
