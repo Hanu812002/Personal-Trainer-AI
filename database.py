9ce5fef630bce61a735e4d6be9109fbcf91b7467
Hanu Agarwal <hanuagarwal181@gmail.com>
2026-04-19 19:57:14 +0530

    changes

--- database.py
+++ database.py
@@ -6,8 +6,8 @@ import streamlit as st
 
 load_dotenv()
 
-SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
